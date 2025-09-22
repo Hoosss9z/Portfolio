e5b3798b0b3e8bc6ff3afb0b13f0eb4280d86d04
Hoosss9z <132957408+Hoosss9z@users.noreply.github.com>
2025-09-22 19:57:41 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="border-t border-white/10 mt-20">
       <div className="container py-10 flex flex-col md:flex-row items-center justify-between gap-4">
-        <p className="text-white/60">© {new Date().getFullYear()} Ton Nom — Tous droits réservés.</p>
+        <p className="text-white/60">© {new Date().getFullYear()} Hussein El Moursi — Tous droits réservés.</p>
         <div className="flex gap-6 text-white/70">
           <Link href="https://github.com/tonpseudo" target="_blank">GitHub</Link>
           <Link href="https://www.linkedin.com/in/hussein-el-moursi-546728206/" target="_blank">LinkedIn</Link>
